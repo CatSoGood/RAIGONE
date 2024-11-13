7bb65b55b8e892a54f93be584e70e000a4fe33ce
BB Chon <105630707+CatSoGood@users.noreply.github.com>
2024-11-14 03:48:51 +0700

    Delete RaiGone/RAIGONE directory

--- RaiGone/RAIGONE/list_templates/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from list_templates import views
-
-urlpatterns = [
-    path('',views.index),
-    path('template',views.template),
-    path('dashboard',views.dashboard)
-]
