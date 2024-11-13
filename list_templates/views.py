fdfaf79504884dea3c35f24d8cccd76ec8f7161d
BellaTrix <chonchanin.boom@gmail.com>
2024-11-14 06:12:35 +0700

    add data table in template.html use block content

--- list_templates/views.py
+++ list_templates/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from list_templates.models import orders
 
+name = "Chonchanin"
 
 def index (request):
-    name = "admin"
-    return render(request,"index.html",{"name":name})
+    return render(request,"index.html")
 
 def template (request):
-    return render(request, "template.html")
+    all_orders = orders.objects.all()
+    return render(request, "template.html", {"name":name, "all_orders":all_orders})
 
 def dashboard(request):
-    return render(request, "dashboard.html")
+    all_orders = orders.objects.all()
+    return render(request, "dashboard.html", {"name":name, "all_orders":all_orders})
